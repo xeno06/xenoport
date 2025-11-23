3a1f93ce18e35724f230a2add2529756d8233b17
xenoxenoxeno0 <vincequijano159@gmail.com>
2025-11-23 23:12:40 +0800

    uploaded

--- binsport/src/Components/Header.jsx
+++ binsport/src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Header.css"; // Ensure exact casing
+import "../Header.css"; // Ensure exact casing
 
 export default function Header() {
   return (
