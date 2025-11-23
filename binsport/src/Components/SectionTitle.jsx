a6b942f49da0d415e37a1083649839cbb4e987ce
xenoxenoxeno0 <vincequijano159@gmail.com>
2025-11-23 23:33:46 +0800

    Uploaded

--- binsport/src/Components/SectionTitle.jsx
+++ binsport/src/Components/SectionTitle.jsx
@@ -1,6 +1,6 @@
 // src/Components/SectionTitle.jsx
 import React from "react";
-import "./section.css"; // Make sure this matches the actual file name
+
 
 export default function SectionTitle({ title }) {
   return <h2 className="section-title">{title}</h2>;
